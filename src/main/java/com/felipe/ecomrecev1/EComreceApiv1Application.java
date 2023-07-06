39895dd87001ebcb101c91b2b8815373613d542f
pitis6 <felipecalvache6@gmail.com>
2023-07-06 15:56:23 -0500

    Implement bean for description of open api

--- src/main/java/com/felipe/ecomrecev1/EComreceApiv1Application.java
+++ src/main/java/com/felipe/ecomrecev1/EComreceApiv1Application.java
@@ -1,7 +1,10 @@
 package com.felipe.ecomrecev1;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EComreceApiv1Application {
@@ -9,5 +12,12 @@ public class EComreceApiv1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(EComreceApiv1Application.class, args);
 	}
-
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI()
+				.info(new Info()
+						.title("audiophile API")
+						.version("1.0")
+						.description("API for audiophile "));
+	}
 }
